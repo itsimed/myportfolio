971a55802c18e8e215cbc186a55a11820055d2fb
itsimed <ie.belouettar@gmail.com>
2025-09-27 22:55:05 +0200

    fix: Simplification du texte du bouton CV
    - Changement de 'Télécharger CV'/'Download CV' vers 'CV' simple
    - Texte plus concis et uniforme dans les deux langues

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -130,7 +130,7 @@ const Hero = () => {
       url: 'https://t.me/itsimedd' 
     },
     { 
-      name: t('hero.downloadCV'), 
+      name: 'CV', 
       icon: (
         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
           <path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z"/>
