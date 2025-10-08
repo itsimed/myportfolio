cac6f46d9cca159f870fce7681cb71d614e72270
itsimed <ie.belouettar@gmail.com>
2025-10-08 15:10:00 +0200

    cv updattee

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -136,8 +136,7 @@ const Hero = () => {
           <path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z"/>
         </svg>
       ), 
-      url: '/CV octobre.pdf',
-      download: true
+      url: 'https://storage.googleapis.com/wiizassets/portfolio%20rayan/CV%20Imed%20Belouettar.pdf',
     },
   ];
 
